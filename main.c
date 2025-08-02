5cb25a4a1d59bf91f4d137439257a201e8b0b7bc
Bryan de Jong <bryan@levitade.io>
2025-08-02 14:55:57 +0200

    feat: hello_world in a function

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+void hello_world(void);
+
 int main() {
-    printf("Hello world!");
+    hello_world();
     return 0;
+}
+
+void hello_world() {
+    printf("Hello world!");
 }
